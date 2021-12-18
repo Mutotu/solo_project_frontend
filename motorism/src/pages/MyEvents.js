cbafdbc8d2722312ace4d50c822c3c1974f9e04b
mutotu <muttalip.aydemir@gmail.com>
2021-12-18 12:15:37 -0800

    Adding css to MyeventsPage

--- motorism/src/pages/MyEvents.js
+++ motorism/src/pages/MyEvents.js
@@ -1,158 +1,9 @@
-// import { useState, useEffect } from "react";
-// import axios from "axios";
-
-// const MyEvents = (props) => {
-//   const [events, setEvents] = useState([]);
-
-//   const getMyEvents = async () => {
-//     const myEvents = await axios.get("http://localhost:3001/events/getsaves", {
-//       headers: {
-//         "content-type": "application/JSON",
-//         Authorization: localStorage.getItem("userId"),
-//       },
-//     });
-//     setEvents(myEvents.data.savedEvents);
-//   };
-
-//   const deleteEvent = async (id) => {
-//     if (id) {
-//       return await axios.delete(`http://localhost:3001/events/delete/${id}`, {
-//         headers: {
-//           "content-type": "application/JSON",
-//           Authorization: localStorage.getItem("userId"),
-//         },
-//       });
-//     }
-//   };
-
-//   const displayEvents = () => {
-//     return events.map((ev, i) => {
-//       return (
-//         <div key={ev.id}>
-//           <li>{ev.name}</li>
-//           <li>{ev.city}</li>
-//           <li>{ev.state}</li>
-//           <li>{ev.date}</li>
-//           <li>{ev.type}</li>
-//           <li>{ev.details}</li>
-//           <button
-//             onClick={() => {
-//               deleteEvent(ev.id);
-//             }}
-//           >
-//             Remove
-//           </button>
-//         </div>
-//       );
-//     });
-//   };
-
-//   useEffect(() => {
-//     getMyEvents();
-//   }, []);
-
-//   return (
-//     <div>
-//       My Events
-//       {events.length === 0 ? <h3>loading...</h3> : <ul>{displayEvents()}</ul>}
-//     </div>
-//   );
-// };
-
-// export default MyEvents;
-
-// import { useState, useEffect,useContext } from "react";
-// import axios from "axios";
-
-// const MyEvents = (props) => {
-//   const [events, setEvents] = useState([]);
-
-//   const [collectId, setCollectId] = useState([]);
-
-//   const getMyEvents = async () => {
-//     const myEvents = await axios.get("http://localhost:3001/events/getsaves", {
-//       headers: {
-//         "content-type": "application/JSON",
-//         Authorization: localStorage.getItem("userId"),
-//       },
-//     });
-
-//     setEvents(myEvents.data.savedEvents);
-
-//     extractId();
-//   };
-//   let idCollector = [];
-//   const extractId = () => {
-//     for (let i of events) {
-//       let id = {};
-//       id[i.id] = true;
-//       idCollector.push(id);
-//     }
-//     setCollectId(idCollector);
-//     // idCollector = [];
-//   };
-//   console.log(collectId);
-//   useEffect(() => {
-//     extractId();
-//   }, [events]);
-//   const deleteEvent = async (id) => {
-//     if (id) {
-//       const deletedEvent = await axios.delete(
-//         `http://localhost:3001/events/delete/${id}`,
-//         {
-//           headers: {
-//             "content-type": "application/JSON",
-//             Authorization: localStorage.getItem("userId"),
-//           },
-//         }
-//       );
-//     }
-
-//     getMyEvents();
-//   };
-
-//   const displayEvents = () => {
-//     return events.map((ev, i) => {
-//       return (
-//         <div key={ev.id}>
-//           <li>{ev.name}</li>
-//           <li>{ev.city}</li>
-//           <li>{ev.state}</li>
-//           <li>{ev.date}</li>
-//           <li>{ev.type}</li>
-//           <li>{ev.details}</li>
-//           <button
-//             onClick={() => {
-//               deleteEvent(ev.id);
-
-//               // console.log("button");
-//             }}
-//           >
-//             Remove
-//           </button>
-//         </div>
-//       );
-//     });
-//   };
-
-//   useEffect(() => {
-//     getMyEvents();
-//   }, []);
-
-//   return (
-//     <div>
-//       My Events
-//       {events.length === 0 ? <h3>loading...</h3> : <ul>{displayEvents()}</ul>}
-//     </div>
-//   );
-// };
-
-// export default MyEvents;
-////////////////////////////////////////with useContedxt
+import "./cssFiles/MyEvents.css";
 import { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { AppContext } from "../context/AppContext";
 import env from "react-dotenv";
+
 const MyEvents = (props) => {
   const [events, setEvents] = useState([]);
   const { idState } = useContext(AppContext);
@@ -206,22 +57,28 @@ const MyEvents = (props) => {
   const displayEvents = () => {
     return events.map((ev, i) => {
       return (
-        <div key={ev.id}>
-          <li>{ev.name}</li>
-          <li>{ev.city}</li>
-          <li>{ev.state}</li>
-          <li>{ev.date}</li>
-          <li>{ev.type}</li>
-          <li>{ev.details}</li>
-          <button
-            onClick={() => {
-              deleteEvent(ev.id);
+        <div>
+          <a href='mailto:email@example.com'>
+            Send Email to the Organizer (UserName)
+          </a>
+          <div key={ev.id} className='event-card-li'>
+            <li>Name of Event: {ev.name}</li>
+            <li>City: {ev.city}</li>
+            <li>State: {ev.state}</li>
+            <li>Date: {ev.date}</li>
+            <li>Type: {ev.type}</li>
+            <li>Details: {ev.details}</li>
+            <button
+              className='event-card-btn'
+              onClick={() => {
+                deleteEvent(ev.id);
 
-              // console.log("button");
-            }}
-          >
-            Remove
-          </button>
+                // console.log("button");
+              }}
+            >
+              Remove
+            </button>
+          </div>
         </div>
       );
     });
@@ -233,11 +90,10 @@ const MyEvents = (props) => {
 
   return (
     <div>
-      My Events
       {events.length === 0 ? (
         <h3>You have no events saved</h3>
       ) : (
-        <ul>{displayEvents()}</ul>
+        <ul className='event-card'>{displayEvents()}</ul>
       )}
     </div>
   );
